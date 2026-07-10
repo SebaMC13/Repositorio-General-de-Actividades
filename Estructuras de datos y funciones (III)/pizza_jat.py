1fb283c6e62e4ca189bbaccfa053a14f7c73e99a
Note Sebastián Muñoz <smc.archivera@gmail.com>
2026-07-10 18:24:20 -0400

    while pizza

--- Estructuras de datos y funciones (III)/pizza_jat.py
+++ Estructuras de datos y funciones (III)/pizza_jat.py
@@ -16,12 +16,36 @@ ingredientes = [
 masa_escogida = " "
 salsa_escogida = " "
 ingrediente_escogido = " "
-# Inicio del programa
-print("PIZZA JAT")
-menu_principal = input(
-    "Esta dentro del menu interactivo de Pizza JAT. Por favor arme su pizza: Masas - Salsas - Ingredientes\n"
-)
 
+# Inicio del programa
+print("- - PIZZA JAT - -\nEsta dentro del menu interactivo de Pizza JAT")
+opcion_del_panel_principal = " "
+while opcion_del_panel_principal != "7":
+    print("1 = Cambiar masa")
+    print("2 = Cambiar salsa")
+    print("3 = Agregar ingrediente")
+    print("4 = Eliminar ingrediente")
+    print("5 = Ver pizza")
+    print("6 = Confirmar pedido")
+    print("7 = Salir")
+    opcion_del_panel_principal = input("Seleccione un numero: ")
+    if opcion_del_panel_principal == "1":
+        print("Escoja una masa")
+        for masa in masas:
+        print(masa)
+    masa_escogida = input("Seleccione una masa: ")
+    elif opcion_del_panel_principal == "2":
+        print("Escoja una salsa")
+    elif opcion_del_panel_principal == "3":
+        print("Escoja los ingredientes")
+    elif opcion_del_panel_principal == "4":
+        print("Elimine ingredientes")
+    elif opcion_del_panel_principal == "5":
+        print("Asi luce la pizza que acaba de preparar")
+    elif opcion_del_panel_principal == "6":
+        print("")
+    else:
+        print("Escoja una opción mostrada en pantalla")
 # elige el tipo de masa
 for masa in masas:
     print(masa)

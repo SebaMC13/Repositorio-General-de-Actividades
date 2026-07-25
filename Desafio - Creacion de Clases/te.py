5991ffe0cdca0d7417f2c7d855cfa0df9597af10
Sebastián Muñoz <smc.archivera@gmail.com>
2026-07-25 17:04:30 -0400

    python 1

---
+++ Desafio - Creacion de Clases/te.py
@@ -0,0 +1,12 @@
+# Paso 1
+class Te:
+    def __init__(self, sabores, formato):
+        self.sabores = sabores
+        self.formato = formato
+
+
+# Agregar metodo estatico que retorne el tiempo de preparación y la recomendación correspondiente, según el sabor ingresado por parámetro.
+
+# Parámetro sabor como un número entero
+
+# Agregue un método estático que retorne el precio según el formato ingresado por parámetro (número entero).
